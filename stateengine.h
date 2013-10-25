9e12b13c4acd59c2f5842b4e1672d1d1bbf9a454
bsparks <bspar145@students.bju.edu>
2013-10-24 21:07:58 -0400

    All headers updated.

--- stateengine.h
+++ stateengine.h
@@ -1,11 +1,27 @@
+/**************************************************************************
+**
+**   stateengine.h
+**
+**   This file copyright 2013 Team Crackpot.
+**   Work done for CpS 111 at Bob Jones University.
+**   Login IDs: afisc855 bspar145 ckost598
+**
+**************************************************************************/
+
 #ifndef STATEENGINE_H
 #define STATEENGINE_H
+
+#include "chests.h"
+#include "llama.h"
+#include "riddle.h"
+#include "ulmainwindow.h"
+#include "world.h"
+#include "worldgenerator.h"
 
 #include <QObject>
 #include <vector>
 #include <QTimer>
 #include <QString>
-#include "llama.h"
 
 class StateEngine : public QObject
 {
