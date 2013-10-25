4352b2f50e5a3e87c7383721abb26017d7135447
Alex Fischer <alex.j.fischer@gmail.com>
2013-10-24 21:00:08 -0400

    Some skeleton work on StateEngine; sorry about the User@bju.edu commits. Those were me.

--- stateengine.h
+++ stateengine.h
@@ -1,4 +1,27 @@
 #ifndef STATEENGINE_H
 #define STATEENGINE_H
 
+#include <QObject>
+#include <vector>
+#include <QTimer>
+#include <QString>
+#include "llama.h"
+
+class StateEngine : public QObject
+{
+    Q_OBJECT
+
+public:
+    StateEngine();
+
+private:
+    static vector<Llama*> llamas;
+    static QString currentRiddle, currentAnswer;
+
+signals:
+    void tick(int numTicks);
+private slots:
+    void on_timer_timeout();
+};
+
 #endif // STATEENGINE_H
